7ce3dae22ab1f233d1534569cca22b74b9bf22f5
Kararin <karina.shatokhina@gmail.com>
2018-05-03 18:24:54 +0300

    message cmp and service added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,15 @@ import Hero from './hero';
 import {Heroes} from './mock-heroes';
 import {Observable} from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of';
+import {MessageService} from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private msgService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.msgService.add('Hero Service: fetched heroes');
     return of(Heroes);
   }
 }
---
+++ src/app/message.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {MessageService} from './message.service';
+
+@Injectable()
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
