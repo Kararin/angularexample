01a6226799631ea354fb0f66e13a06b691513f35
Kararin <karina.shatokhina@gmail.com>
2018-05-11 18:11:57 +0300

    detail router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router} from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [{
   path: 'heroes', component: HeroesComponent
@@ -9,6 +10,8 @@ const routes: Routes = [{
   path: 'dashboard', component: DashboardComponent
 }, { 
   path: '', redirectTo: '/dashboard', pathMatch: 'full'
+}, {
+  path: 'detail/:id', component: HeroDetailComponent
 }]
 @NgModule({
   exports: [RouterModule],
